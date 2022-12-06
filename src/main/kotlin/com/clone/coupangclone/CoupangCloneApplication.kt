3c12e490416eb267fbfcc2363e0af3463988cd22
mjhstar <mjh_star@naver.com>
2022-12-06 13:58:16 +0900

    init commit

---
+++ src/main/kotlin/com/clone/coupangclone/CoupangCloneApplication.kt
@@ -0,0 +1,11 @@
+package com.clone.coupangclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoupangCloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoupangCloneApplication>(*args)
+}
---
+++ src/test/kotlin/com/clone/coupangclone/CoupangCloneApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.clone.coupangclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoupangCloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
